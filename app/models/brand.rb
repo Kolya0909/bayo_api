2b6d96039ce4036944318c1a3ee13a622d5c60a6
Kolya0909 <bond410@ukr.net>
2023-01-15 00:18:54 +0200

    create_controllers_logic_for_product_categories_ratins

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,9 @@
 class Brand < ApplicationRecord
+
+  acts_as_api
+
+  validates :name, presence: true
+
+  has_many :products, dependent: :destroy
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,13 @@ class Product < ApplicationRecord
   acts_as_api
 
   enum product_status: %i[sold in_stock delivery_expected]
-  enum product_type: %i[telephon smartphon tablet tv]
+  enum product_type: %i[telephon smartphon tablet tv laptop]
 
   validates :name, presence: true
 
   belongs_to :company
+  belongs_to :brand
+
   has_one :product_info, dependent: :destroy
 
   has_and_belongs_to_many :categories, -> { distinct }
