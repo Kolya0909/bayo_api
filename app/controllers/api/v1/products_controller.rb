92beda8ccaedf30a30bd748fc46b6c58fe147dae
Kolya0909 <bond410@ukr.net>
2023-02-22 17:20:15 +0200

    start_creating_update_action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -54,6 +54,31 @@ module Api
         render_success service.product.as_api_response(:list)
       end
 
+      swagger_api :update do
+        summary "Create one Product"
+        param :header, :authtoken, :string, :required, "Main Admin authtoken"
+        param :form, :name, :string, :required, "Product name"
+        param :form, :price, :string, :required, "Product price"
+        param :form, :product_type, :string, :required, "telephon | smartphon | tablet | tv | laptop"
+        param :form, :product_status, :string, :required, "in_stock | sold | delivery_expected"
+        param :form, :category_ids, :string, :optional, "Category identifiers ',' separate"
+        param :form, :brand_id, :string, :optional, "Brand identifier"
+        param :form, :company_id, :string, :optional, "Company identifier"
+        param :form, :title, :string, :optional, "Product info title"
+        param :form, :description, :string, :optional, "Product info description"
+        param :form, :screen, :string, :optional, "Product info screen"
+        param :form, :processor, :string, :optional, "Product info processor"
+        param :form, :ram, :string, :optional, "Product info ram"
+        param :form, :memory, :string, :optional, "Product info memory"
+        param :form, :camera, :string, :optional, "Product info camera"
+        response :ok, "Success"
+      end
+
+      def update
+        current_main_admin_must_be && return
+
+      end
+
     end
   end
 end
