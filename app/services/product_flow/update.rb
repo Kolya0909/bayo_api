ff8e648494f635f96f37be7cb9d1314d8d1a1424
Kolya0909 <bond410@ukr.net>
2023-05-25 10:26:24 +0300

    changed_product_flow_update

--- app/services/product_flow/update.rb
+++ app/services/product_flow/update.rb
@@ -5,17 +5,19 @@ module ProductFlow
 
     def initialize(params)
       @params = params
+      @product = Product.find_by(id: params[:id])
     end
 
     def call
       update_product!
+      update_product_info!
     end
 
     private
-    attr_reader :options, :params
+    attr_reader :product_options, :product_info_options, :params
 
-    def options
-      @options ||= {
+    def product_options
+      @product_options ||= {
         name: params[:name] || product.name,
         price: params[:price] || product.price,
         product_type: params[:product_type] || product.product_type,
@@ -23,21 +25,27 @@ module ProductFlow
         category_ids: params[:category_ids] || product.category_ids,
         brand_id: params[:brand_id] || product.brand_id,
         company_id: params[:company_id] || product.company_id,
-        title: params[:title] || product.title,
-        screen: params[:screen] || product.screen,
-        processor: params[:processor] || product.processor,
-        ram: params[:ram] || product.ram,
-        memory: params[:memory] || product.memory,
-        camera: params[:camera] || product.camera,
+      }
+    end
+
+    def product_info_options
+      @product_info_options ||= {
+        title: params[:title] || product.product_info.title,
+        description: params[:description] || product.product_info.description,
+        screen: params[:screen] || product.product_info.screen,
+        processor: params[:processor] || product.product_info.processor,
+        ram: params[:ram] || product.product_info.ram,
+        memory: params[:memory] || product.product_info.memory,
+        camera: params[:camera] || product.product_info.camera,
       }
     end
 
     def update_product!
-      product.update!(options)
+      product.update!(product_options)
     end
 
-    def product
-      @product ||= Product.find_by(id: params[:id])
+    def update_product_info!
+      product.product_info.update!(product_info_options)
     end
   end
 end
